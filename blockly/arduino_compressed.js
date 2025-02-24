e13c6a3af6f4346f755174bedc4814319aa5062a
zahra shahrooei <115218048+zahrashahrooeii@users.noreply.github.com>
2025-02-24 13:21:37 +0100

    Added debugging enhancements and UI improvements

--- blockly/arduino_compressed.js
+++ blockly/arduino_compressed.js
@@ -55,3 +55,14 @@ Blockly.Arduino.procedures_callnoreturn=function(){for(var a=Blockly.Arduino.var
 Blockly.Arduino.procedures_ifreturn=function(){var a="if ("+(Blockly.Arduino.valueToCode(this,"CONDITION",Blockly.Arduino.ORDER_NONE)||"false")+") {\n";if(this.hasReturnValue_)var b=Blockly.Arduino.valueToCode(this,"VALUE",Blockly.Arduino.ORDER_NONE)||"null",a=a+("  return "+b+";\n");else a+="  return;\n";return a+"}\n"};Blockly.Arduino.texts={};Blockly.Arduino.text=function(){return[Blockly.Arduino.quote_(this.getFieldValue("TEXT")),Blockly.Arduino.ORDER_ATOMIC]};Blockly.Arduino.variables={};Blockly.Arduino.variables_get=function(){return[Blockly.Arduino.variableDB_.getName(this.getFieldValue("VAR"),Blockly.Variables.NAME_TYPE),Blockly.Arduino.ORDER_ATOMIC]};
 Blockly.Arduino.variables_declare=function(){this.getFieldValue("TYPE");var a=Blockly.Arduino.valueToCode(this,"VALUE",Blockly.Arduino.ORDER_ASSIGNMENT)||"0",b=Blockly.Arduino.variableDB_.getName(this.getFieldValue("VAR"),Blockly.Variables.NAME_TYPE);Blockly.Arduino.setups_["setup_var"+b]=b+" = "+a+";\n";return""};
 Blockly.Arduino.variables_set=function(){var a=Blockly.Arduino.valueToCode(this,"VALUE",Blockly.Arduino.ORDER_ASSIGNMENT)||"0";return Blockly.Arduino.variableDB_.getName(this.getFieldValue("VAR"),Blockly.Variables.NAME_TYPE)+" = "+a+";\n"};
+// Define the generator for the setup block
+Blockly.Arduino['setup'] = function(block) {
+    var statements = Blockly.Arduino.statementToCode(block, 'DO');
+    return 'void setup() {\n' + statements + '}\n\n';
+};
+
+// Define the generator for the loop block
+Blockly.Arduino['loop'] = function(block) {
+    var statements = Blockly.Arduino.statementToCode(block, 'DO');
+    return 'void loop() {\n' + statements + '}\n\n';
+};
--- blockly/blocks_compressed.js
+++ blockly/blocks_compressed.js
@@ -202,3 +202,26 @@ Blockly.Blocks.variables_get={init:function(){this.setHelpUrl(Blockly.Msg.VARIAB
 this.setFieldValue(b,"VAR")},contextMenuType_:"variables_set",customContextMenu:function(a){var b={enabled:!0},c=this.getFieldValue("VAR");b.text=this.contextMenuMsg_.replace("%1",c);c=goog.dom.createDom("field",null,c);c.setAttribute("name","VAR");c=goog.dom.createDom("block",null,c);c.setAttribute("type",this.contextMenuType_);b.callback=Blockly.ContextMenu.callbackFactory(this,c);a.push(b)}};
 Blockly.Blocks.variables_set={init:function(){this.jsonInit({message0:Blockly.Msg.VARIABLES_SET,args0:[{type:"field_variable",name:"VAR",variable:Blockly.Msg.VARIABLES_DEFAULT_NAME},{type:"input_value",name:"VALUE"}],previousStatement:null,nextStatement:null,colour:Blockly.Blocks.variables.HUE,tooltip:Blockly.Msg.VARIABLES_SET_TOOLTIP,helpUrl:Blockly.Msg.VARIABLES_SET_HELPURL});this.contextMenuMsg_=Blockly.Msg.VARIABLES_SET_CREATE_GET},getVars:function(){return[this.getFieldValue("VAR")]},renameVar:function(a,
 b){Blockly.Names.equals(a,this.getFieldValue("VAR"))&&this.setFieldValue(b,"VAR")},contextMenuType_:"variables_get",customContextMenu:Blockly.Blocks.variables_get.customContextMenu};
+Blockly.Blocks['setup'] = {
+  init: function() {
+      this.appendDummyInput()
+          .appendField("Setup");
+      this.appendStatementInput("DO")
+          .setCheck(null);
+      this.setColour(290);
+      this.setTooltip("Runs once at the beginning.");
+      this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['loop'] = {
+  init: function() {
+      this.appendDummyInput()
+          .appendField("Loop");
+      this.appendStatementInput("DO")
+          .setCheck(null);
+      this.setColour(290);
+      this.setTooltip("Runs continuously after setup.");
+      this.setHelpUrl("");
+  }
+};
